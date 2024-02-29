31d1093889599f1debd393914261127795742b11
laila zak <lailazak522@gmail.com>
2024-02-29 20:33:31 +0300

    add todo box and sytle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  const handleAddTodo = () => {
+    const text = inputRef.current.value.trim();
+    if (text) {
+      const newItem = { id: Date.now(), completed: false, text };
+      setTodos([...todos, newItem]);
+      inputRef.current.value = "";
+      localStorage.setItem("todos", JSON.stringify([...todos, newItem]));
+    }
+  };
+
+  const handleItemDone = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  };
+
+  const handleDeleteItem = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>ToDo List</h2>
+      <div className="to-do-container">
+        <ul>
+          {todos.map(({ id, text, completed }) => (
+            <div className="item" key={id}>
+              <li
+                className={completed ? "done" : ""}
+                onClick={() => handleItemDone(id)}
+              >
+                {text}
+              </li>
+              <span onClick={() => handleDeleteItem(id)} className="trash">
+                ❌
+              </span>
+            </div>
+          ))}
+        </ul>
+        <input ref={inputRef} placeholder="Enter item..." />
+        <div className="btn">
+          <button onClick={handleAddTodo}>Add</button>
+        </div>
+      </div>
     </div>
   );
 }
